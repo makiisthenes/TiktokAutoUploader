f9fb016a0f312f1f8a20f08b7cdf3703392a2292
Michael Peres <52138450+makiisthenes@users.noreply.github.com>
2021-04-25 17:41:16 +0100

    Update Video.py

--- Video.py
+++ Video.py
@@ -53,26 +53,3 @@ class Video:
         self.clip.write_videofile(self.dir, fps=24)
 
 
-
-if __name__ == "__main__":
-    # test code
-    # video = Video(r"VideosDirPath\test.mp4", "")
-    # video.customCrop(0, 2)
-
-    # Testing using ColorClip class in MoviePy. Procedural Method
-    # https://zulko.github.io/moviepy/getting_started/videoclips.html
-    meme_clip = (VideoFileClip("example.mp4").resize(width=1080))  # Ignore error.
-    base_clip = ColorClip(size=(1080, 1920), color=[10, 10, 10], duration=meme_clip.duration)
-    OFFSET = -20  # 30
-    bottom_meme_pos = 960 + (((1080 / meme_clip.size[0]) * (meme_clip.size[1])) / 2) + OFFSET
-    top_meme_pos = 960 - (((1080 / meme_clip.size[0]) * (meme_clip.size[1])) / 2) - OFFSET
-    to_pos = 1920 / 6
-    print(bottom_meme_pos)  # this is position of bottom y of meme on video clip.
-    memeText = "This is so funny and so long that it wraps to the other side."
-    memeOverlay = TextClip(txt=memeText, bg_color="black", color="white", size=(900, None), kerning=-1,
-                           method="caption", font="Arial", fontsize=80,
-                           align="center")  # stroke_color="black", stroke_width=2.5
-    memeOverlay = memeOverlay.set_duration(meme_clip.duration)
-    video = CompositeVideoClip([base_clip, meme_clip.set_position(("center", "center")),
-                                memeOverlay.set_position(("center", bottom_meme_pos))])
-    video.write_videofile("edited.mp4", fps=24)
